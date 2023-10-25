e6a73feaa8621a30c6e82bd399c42c02b04ea788
Emiliano Hurtado <emi.hurtado@nokia.com>
2023-10-25 11:22:54 -0600

    Version Final

--- Practica 2/Practica2Python/cliente.py
+++ Practica 2/Practica2Python/cliente.py
@@ -2,17 +2,17 @@ import socket
 import os
 import time
 
-def main():
+def send_file(client_socket): #////////////////////////////////////////////////
     # Configuración del cliente
     host = '127.0.0.1'
     port = 12345
     buffer_size = 30720
 
     # Carpeta de archivos del cliente
-    client_folder = "C:/Users/Dell/Desktop/ESCOM/Redes 2/Duplex-ACR/FlujoArchivo/Practica 2/Practica2Python/ArchivosCliente/"
+    client_folder = "C:/ArchivosCliente/"
 
-    # Crear un socket UDP
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+     # Enviar una solicitud al servidor para recibir un archivo
+    client_socket.sendto(b"SEND", (host, port))
 
     # Solicitar al usuario el nombre del archivo a enviar
     file_name = input("Ingrese el nombre del archivo a enviar: ")
@@ -51,6 +51,31 @@ def main():
         print("Archivo enviado con éxito")
     else:
         print(f"El archivo '{file_name}' no existe en la ubicación especificada.")
+    input("Presiona Enter...")
+    os.system('cls')
+
+def main():
+    # Configuración del cliente
+    host = '127.0.0.1'  # Dirección IP del servidor
+    port = 12345       # Puerto del servidor
+
+    # Crear un socket UDP
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    while True:
+        print("Menu:")
+        print("1. Enviar un archivo al servidor")
+        print("2. Salir")
+
+        choice = input("Elija una opción (1/2): ")
+
+        if choice == '1':
+            send_file(client_socket)
+        elif choice == '2':
+            print("Gracias por usarnos. Hasta la próxima...")
+            break
+        else:
+            print("Opción no válida. Intente de nuevo.")
 
     client_socket.close()
 
--- Practica 2/Practica2Python/server.py
+++ Practica 2/Practica2Python/server.py
@@ -1,18 +1,15 @@
 import socket
 import os
+import time
 
-def main():
+def receive_file(server_socket): #////////////////////////////////////////////
     # Configuración del servidor
     host = '127.0.0.1'
     port = 12345
     buffer_size = 65535
 
     # Carpeta de archivos del servidor
-    server_folder = "C:/Users/Dell/Desktop/ESCOM/Redes 2/Duplex-ACR/FlujoArchivo/Practica 2/Practica2Python/ArchivosServidor/"
-
-    # Crear un socket UDP y enlace
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server_socket.bind((host, port))
+    server_folder = "C:/ArchivosServidor/"
 
     packet_number = 0
     received_filename = None
@@ -34,6 +31,29 @@ def main():
         server_socket.sendto(b"ACK", client_address)  # Enviar confirmación al cliente
 
     print(f"Archivo '{received_filename}' recibido con éxito")
+    server_socket.sendto(b"ACK", client_address)  # Enviar confirmación al cliente
+
+def main():
+    # Configuración del servidor
+    host = '127.0.0.1'  # Dirección IP en la que el servidor escuchará
+    port = 12345       # Puerto en el que el servidor escuchará
+    buffer_size = 65535  # Tamaño del búfer para la lectura/escritura de datos
+
+    # Crear un socket UDP y enlazarlo
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind((host, port))
+
+    client_address = None
+
+    while True:
+        data, client_address = server_socket.recvfrom(buffer_size)
+        if not data:
+            break
+        if data == b"SEND":
+            receive_file(server_socket)
+        """elif data == b"RECEIVE":
+            send_file(server_socket)"""
+
     server_socket.close()
 
 if __name__ == "__main__":
